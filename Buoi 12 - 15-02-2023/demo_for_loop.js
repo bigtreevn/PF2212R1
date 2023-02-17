327d907db419b19fa569b915ab80b5badca8a210
bigtreevn <namhut07@gmail.com>
2023-02-17 21:32:07 +0700

    Lesson 12

---
+++ Buoi 12 - 15-02-2023/demo_for_loop.js
@@ -0,0 +1,16 @@
+// In ra màn hình các số từ 1 đến 10
+//for (let i = 1; i < 11; i++) {
+//    document.write(i + '<br>');
+//}
+// In ra màn hình các số từ 10 đến 1
+//for (let i = 10; i > 0; i--) {
+ //   document.write(i + '<br>');
+//}
+// In ra màn hình các số từ 1.0 đến 1.9
+//for (let i = 1.0; i < 2; i += 0.1) {
+//    document.write(i + '<br>');
+//}
+// In ra màn hình các số chẵn từ 0 đến 100
+for (let i = 0; i <= 50; i ++) {
+    document.write(i*2 + '<br>');
+}
